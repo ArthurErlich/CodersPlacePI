c295217053b3e44e3ddd334aec08eddf1c3ad438
ArthurErlich <a.erlich@live.de>
2024-10-01 09:32:31 +0200

    changed position of elements to be more logical

--- frontend/code/CustomElements.ts
+++ frontend/code/CustomElements.ts
@@ -1,16 +1,4 @@
 namespace codersPlace {
-    class FooterElement extends HTMLElement{
-        constructor(){
-            super();
-        }
-        connectedCallback() {
-            this.innerHTML = `
-            <div><a href="https://github.com/ArthurErlich">GitHub</a></div>
-            <div><a href="/frontend/html/salary_transparency.html">Salary Transparency</a></div>
-            <div><a href="/frontend/html/changelog.html">changelog</a></div>
-            `;
-        }
-    }
     class NaviElement extends HTMLElement{
         constructor(){
             super();
@@ -20,8 +8,21 @@ namespace codersPlace {
             <nav>
                 <a href="/frontend/html/index.html"><div>Home</div></a>
                 <a href="/frontend/html/blog/blog_index.html"><div>Blog</div></a>
+                <a href="/frontend/html
                 <a href="/frontend/html/about_me.html"><div>About Me</div></a>
             </nav>
+            `;
+        }
+    }
+    class FooterElement extends HTMLElement{
+        constructor(){
+            super();
+        }
+        connectedCallback() {
+            this.innerHTML = `
+            <div><a href="https://github.com/ArthurErlich">GitHub</a></div>
+            <div><a href="/frontend/html/salary_transparency.html">Salary Transparency</a></div>
+            <div><a href="/frontend/html/changelog.html">changelog</a></div>
             `;
         }
     }
