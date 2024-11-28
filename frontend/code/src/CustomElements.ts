5f6f6761187edd7b4df70d89f9bec804b1bd6869
ArthurErlich <a.erlich@live.de>
2024-11-28 19:28:45 +0100

    reoarder imports

--- frontend/code/src/CustomElements.ts
+++ frontend/code/src/CustomElements.ts
@@ -1,10 +1,10 @@
 
-import { DialogElement } from "./customElements/DialogElement.js";
-import { FooterElement } from "./customElements/FooterElement.js";
 import { NaviElement } from "./customElements/NaviElement.js";
+import { FooterElement } from "./customElements/FooterElement.js";
+import { DialogElement } from "./customElements/DialogElement.js";
 import { IndexLinkingElement } from "./customElements/IndexLinkingElement.js";
 
-customElements.define("custom-footer", FooterElement);
 customElements.define("custom-header", NaviElement);
+customElements.define("custom-footer", FooterElement);
 customElements.define("custom-img-dialog", DialogElement)
 customElements.define("custom-index-links", IndexLinkingElement)
