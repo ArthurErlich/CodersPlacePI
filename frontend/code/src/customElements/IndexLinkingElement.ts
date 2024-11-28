131fa91c31ccde89fc6ffa53344ecb8bc9bea6b3
ArthurErlich <a.erlich@live.de>
2024-11-28 06:49:57 +0100

    retrieve header elements in the correct order

--- frontend/code/src/customElements/IndexLinkingElement.ts
+++ frontend/code/src/customElements/IndexLinkingElement.ts
@@ -3,9 +3,22 @@ export class IndexLinkingElement extends HTMLElement {
         super();
     }
     connectedCallback() {
+        window.addEventListener("load", (event) => {
+            this.getHTagList(["h1","h2","h3"]);
+        });
+    }
+    private getHTagList(selection:Array<string>): NodeListOf<Element> {
+
+        let selectionString = selection.toString();
+        // retrieves the elements in the correct order
+        let elementCollection: NodeListOf<Element> = document.querySelectorAll(selectionString);
+
+        //TODO:Create Logger interface
+        console.info(elementCollection);
+
+        return elementCollection;
 
     }
-
     // Find all h1 -> h2 and h3 Elements.
     // Add Anker with indexnumber and name to hx tag
     // Create list of Links and stye them like the blog. 
