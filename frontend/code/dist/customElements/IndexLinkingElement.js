e98a91f09851d4a48a13321ad4d88c297cc0d7cd
ArthurErlich <a.erlich@live.de>
2024-11-28 07:15:11 +0100

    added anchor creation

--- frontend/code/dist/customElements/IndexLinkingElement.js
+++ frontend/code/dist/customElements/IndexLinkingElement.js
@@ -4,15 +4,23 @@ export class IndexLinkingElement extends HTMLElement {
     }
     connectedCallback() {
         window.addEventListener("load", (event) => {
-            this.getHTagList(["h1", "h2", "h3"]);
+            console.log("Window loaded, starting with Index creation,");
+            let headerList = this.getHTagList(["h1", "h2", "h3"]);
+            this.setAnkerPoint(headerList);
         });
     }
     getHTagList(selection) {
         let selectionString = selection.toString();
         let elementCollection = document.querySelectorAll(selectionString);
-        console.info(elementCollection);
+        console.log("Found " + elementCollection.length + " Header Elements");
+        console.log(elementCollection);
         return elementCollection;
     }
     setAnkerPoint(headerList) {
+        console.log("Adding anchor points");
+        headerList.forEach((header, key) => {
+            header.id = header.innerText.trim().replace(" ", "-") + "-" + key;
+            console.log(header.id);
+        });
     }
 }
--- frontend/code/src/customElements/IndexLinkingElement.ts
+++ frontend/code/src/customElements/IndexLinkingElement.ts
@@ -4,25 +4,34 @@ export class IndexLinkingElement extends HTMLElement {
     }
     connectedCallback() {
         window.addEventListener("load", (event) => {
-            this.getHTagList(["h1","h2","h3"]);
+            console.log("Window loaded, starting with Index creation,")
+            let headerList:NodeListOf<HTMLElement> = this.getHTagList(["h1", "h2", "h3"]);
+            this.setAnkerPoint(headerList);
+            //TODO fll this.body with correct index, check older Blog for reference
         });
     }
-    private getHTagList(selection:Array<string>): NodeListOf<HTMLElement> {
+    private getHTagList(selection: Array<string>): NodeListOf<HTMLElement> {
 
         let selectionString = selection.toString();
         // retrieves the elements in the correct order
         let elementCollection: NodeListOf<HTMLElement> = document.querySelectorAll(selectionString);
 
         //TODO:Create Logger interface
-        console.info(elementCollection);
+        console.log("Found " + elementCollection.length + " Header Elements");
+        console.log(elementCollection);
 
         return elementCollection;
 
     }
-    private setAnkerPoint(headerList:NodeListOf<HTMLElement>){
+    private setAnkerPoint(headerList: NodeListOf<HTMLElement>): void {
+        console.log("Adding anchor points");
+        headerList.forEach((header, key) => {
+            header.id = header.innerText.trim().replace(" ", "-") + "-" + key;
+            console.log(header.id);
+        });
         
     }
     // Find all h1 -> h2 and h3 Elements. 👍
-    // Add Anker with indexnumber and name to hx tag
+    // Add Anker with indexnumber and name to header tag 👍
     // Create list of Links and stye them like the blog. 
 }
