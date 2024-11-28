5e48e75ee77514901d1decc1ee3f80ea03628103
ArthurErlich <a.erlich@live.de>
2024-11-28 06:49:12 +0100

    rebuilding javascript

--- frontend/code/dist/customElements/IndexLinkingElement.js
+++ frontend/code/dist/customElements/IndexLinkingElement.js
@@ -3,5 +3,14 @@ export class IndexLinkingElement extends HTMLElement {
         super();
     }
     connectedCallback() {
+        window.addEventListener("load", (event) => {
+            this.getHTagList(["h1", "h2", "h3"]);
+        });
+    }
+    getHTagList(selection) {
+        let selectionString = selection.toString();
+        let elementCollection = document.querySelectorAll(selectionString);
+        console.info(elementCollection);
+        return elementCollection;
     }
 }
