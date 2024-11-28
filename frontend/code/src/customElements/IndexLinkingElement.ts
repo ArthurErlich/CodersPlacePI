8fc886989d18ad507db6b3b1f0a11be610bb2cb1
ArthurErlich <a.erlich@live.de>
2024-11-28 07:01:46 +0100

    change element tp HTMLElement

--- frontend/code/src/customElements/IndexLinkingElement.ts
+++ frontend/code/src/customElements/IndexLinkingElement.ts
@@ -7,11 +7,11 @@ export class IndexLinkingElement extends HTMLElement {
             this.getHTagList(["h1","h2","h3"]);
         });
     }
-    private getHTagList(selection:Array<string>): NodeListOf<Element> {
+    private getHTagList(selection:Array<string>): NodeListOf<HTMLElement> {
 
         let selectionString = selection.toString();
         // retrieves the elements in the correct order
-        let elementCollection: NodeListOf<Element> = document.querySelectorAll(selectionString);
+        let elementCollection: NodeListOf<HTMLElement> = document.querySelectorAll(selectionString);
 
         //TODO:Create Logger interface
         console.info(elementCollection);
@@ -19,7 +19,10 @@ export class IndexLinkingElement extends HTMLElement {
         return elementCollection;
 
     }
-    // Find all h1 -> h2 and h3 Elements.
+    private setAnkerPoint(headerList:NodeListOf<HTMLElement>){
+        
+    }
+    // Find all h1 -> h2 and h3 Elements. 👍
     // Add Anker with indexnumber and name to hx tag
     // Create list of Links and stye them like the blog. 
 }
