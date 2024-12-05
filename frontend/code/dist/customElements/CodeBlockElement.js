bdb3a33fab293e7507646038f07d385003394bcb
ArthurErlich <a.erlich@live.de>
2024-12-05 09:11:26 +0100

    added custom code block

--- frontend/code/dist/customElements/CodeBlockElement.js
+++ frontend/code/dist/customElements/CodeBlockElement.js
@@ -8,24 +8,30 @@ export class CodeBlockElement extends HTMLElement {
     getFormattedContent() {
         let text = this.innerHTML;
         let rawTextLines = text.split("\n");
-        let leadingWhiteSpace;
-        this.removeLineIfEmpty(rawTextLines, 0);
-        leadingWhiteSpace = rawTextLines[0].search(/\S|$/);
-        for (let i = 0; i < rawTextLines.length; i++) {
-            rawTextLines[i] = rawTextLines[i].slice(leadingWhiteSpace);
-        }
-        this.removeLineIfEmpty(rawTextLines, rawTextLines.length - 1);
-        for (let i = 0; i < rawTextLines.length; i++) {
-            rawTextLines[i] += "<br>";
-        }
+        rawTextLines = this.removeLineIfEmpty(rawTextLines, 0);
+        rawTextLines = this.replaceLeadingWhitespace(rawTextLines);
+        rawTextLines = this.removeLineIfEmpty(rawTextLines, rawTextLines.length - 1);
+        rawTextLines = this.preserveCodeIndentation(rawTextLines);
         text = rawTextLines.join("");
         this.innerHTML = text;
-        return text;
     }
     removeLineIfEmpty(textLines, index) {
-        console.log("Removing line " + textLines[index]);
         if (textLines[index] == "") {
             textLines.splice(index, 1);
+        }
+        return textLines;
+    }
+    replaceLeadingWhitespace(textLines) {
+        let leadingWhiteSpace;
+        leadingWhiteSpace = textLines[0].search(/\S|$/);
+        for (let i = 0; i < textLines.length; i++) {
+            textLines[i] = textLines[i].slice(leadingWhiteSpace);
+        }
+        return textLines;
+    }
+    preserveCodeIndentation(textLines) {
+        for (let i = 0; i < textLines.length; i++) {
+            textLines[i] += "<br>";
         }
         return textLines;
     }
