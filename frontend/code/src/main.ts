91f559f30ce2f79eaae53e191e04e3bb2e96c6b4
ArthurErlich <a.erlich@live.de>
2024-12-06 21:04:33 +0100

    changed to cosnt

--- frontend/code/src/main.ts
+++ frontend/code/src/main.ts
@@ -1,5 +1,6 @@
 import { FullScreenImage } from "./modules/FullScreenImage.js";
 import { MouseGlow } from "./modules/MouseGlow.js";
+import { TypeWriter } from "./modules/TypeWriter.js";
 
 const  glowEffectSites = ["index.html"]
 const filePath = new URL(document.URL).pathname;
@@ -8,6 +9,7 @@ const fileName = filePath.split("/").pop();
 // Load only when window is finish
 window.onload = function () {
     new FullScreenImage();
+    new TypeWriter();
 }
 if(glowEffectSites.includes(fileName)){
     new MouseGlow();
